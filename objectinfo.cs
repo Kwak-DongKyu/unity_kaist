320f03678fab736dff406fe16f7be7b25e744df3
Kwak-DongKyu <91858707+Kwak-DongKyu@users.noreply.github.com>
2022-07-21 14:50:24 +0900

    secondcommit

---
+++ objectinfo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectinfo : MonoBehaviour
+{
+    public int inertia = 2;
+    public int viscosity = 3;
+    public int elasticity = 4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
